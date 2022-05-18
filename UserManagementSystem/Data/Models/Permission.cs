6f232ec37fe5bafd89e5f0f4b593f329aa0eb3d6
zorka11 <markozorkic1@gmail.com>
2022-05-18 02:01:24 +0200

    Added init models

---
+++ UserManagementSystem/Data/Models/Permission.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace UserManagementSystem.Data.Models
+{
+    public class Permission
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public List<UserPermission> Users { get; set; }
+    }
+}
---
+++ UserManagementSystem/Data/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UserManagementSystem.Data.Models;
+
+namespace UserManagementSystem.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool StatusIsActive { get; set; }
+        public List<UserPermission> Permissions { get; set; }
+
+    }
+}
