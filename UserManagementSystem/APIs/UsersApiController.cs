3858b29467bdc3f302c9a879bd9c909516f7a9ea
Marko Zorkic <marko.zorkic@prointer.ba>
2022-05-19 18:51:42 +0200

    Creating ApiController

---
+++ UserManagementSystem/APIs/UsersApiController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserManagementSystem.DTOs;
+using UserManagementSystem.Repository;
+
+namespace UserManagementSystem.APIs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersApiController : ControllerBase
+    {
+        IUsersRepository _usersRepository;
+        public UsersApiController(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        [HttpGet]
+       // [NoCache]
+        [ProducesResponseType(typeof(List<UserResponseModel>), 200)]
+        [ProducesResponseType(typeof(ApiResponse), 400)]
+        public async Task<ActionResult> Customers()
+        {
+            try
+            {
+                var customers = await _CustomersRepository.GetCustomersAsync();
+                return Ok(customers);
+            }
+            catch (Exception exp)
+            {
+                _Logger.LogError(exp.Message);
+                return BadRequest(new ApiResponse { Status = false });
+            }
+        }
+    }
+}
